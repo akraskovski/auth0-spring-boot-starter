aadd27fb7debea2b8aedb69a8ad305d8d021827a
akraskovski <artem.kraskovski@gmail.com>
2018-01-19 22:19:48 +0300

    initial project structure

---
+++ src/main/java/com/kraskovski/auth0/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.kraskovski.auth0.config;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration class using Auth0.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.apiAudience}")
+    private String apiAudience;
+    @Value("${auth0.issuer}")
+    private String issuer;
+    @Value("${auth0.clientSecret}")
+    private String secret;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer
+                .forHS256(apiAudience, issuer, secret.getBytes())
+                .configure(http)
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}
