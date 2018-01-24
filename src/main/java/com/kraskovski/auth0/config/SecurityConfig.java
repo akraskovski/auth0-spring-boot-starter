a149764b7caa0e1d4a38fa50f05df719559f4596
Artsiom Kraskouski <artem.kraskovski@gmail.com>
2018-01-24 18:04:19 +0300

    removed unused dependencies

--- src/main/java/com/kraskovski/auth0/config/SecurityConfig.java
+++ src/main/java/com/kraskovski/auth0/config/SecurityConfig.java
@@ -38,6 +38,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().permitAll();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
-
     }
 }
--- src/main/java/com/kraskovski/auth0/controller/ErrorController.java
+++ src/main/java/com/kraskovski/auth0/controller/ErrorController.java
@@ -3,10 +3,8 @@ package com.kraskovski.auth0.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
 
 @SuppressWarnings("unused")
 @Controller
@@ -16,8 +14,8 @@ public class ErrorController implements org.springframework.boot.autoconfigure.w
 
     private static final String PATH = "/error";
 
-    @RequestMapping("/error")
-    protected String error(final RedirectAttributes redirectAttributes) throws IOException {
+    @GetMapping("/error")
+    protected String error(final RedirectAttributes redirectAttributes) {
         logger.error("Handling error");
         redirectAttributes.addFlashAttribute("error", true);
         return "redirect:/login";
